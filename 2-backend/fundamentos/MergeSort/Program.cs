699d50e3245a0de40f0bd5939a916334ef24c207
Francisco de Paula <franciscodepaula.web@gmail.com>
2023-08-09 23:56:07 -0300

    Anotações twitter e alura.

---
+++ 2-backend/fundamentos/MergeSort/Program.cs
@@ -0,0 +1,20 @@
+﻿void Mesclar (List<int> lista, List<int> listaEsquerda, List<int> listaDireita)
+{
+
+}
+
+List<int> MergeSort(List<int> lista)
+{
+    if (lista.Count() == 1)
+    {
+        return lista;
+    }
+
+    int meio = lista.Count / 2;
+
+    List<int> listaEsquerda;
+    List<int> listaDireita;
+
+    l  = lista[:meio];
+    listaDireita = 
+}
